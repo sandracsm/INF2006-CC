a5a629758cf06fd4140a328430d9e9ae59a9091c
Sandra C <schoysm@gmail.com>
2025-02-17 14:15:30 +0800

    hi alyssa

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ import os
 
 load_dotenv()  # This loads environment variables from a .env file
 
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5uck@localhost/task_db" # LOCAL
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5uck@10.0.14.152/task_db" # AWS
+
 class Config:
     SECRET_KEY = "inf2006"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:admin@localhost/task_db"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5uck@10.0.14.152/task_db" # AWS
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = "filesystem"  # Store session data in local files
     SESSION_PERMANENT = False  # Session will expire when the browser closes
