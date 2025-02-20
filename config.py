6970a78e2999a3eb808b7e8b2c31776f674ad704
Teo-Hui-Xin-Mandy <2202282@sit.singaporetech.edu.sg>
2025-02-20 11:34:58 +0800

    rename the database

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 load_dotenv()  # This loads environment variables from a .env file
 
-# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5uck@localhost/task_db" # LOCAL
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5uck@localhost/tmdb" # LOCAL
 
 class Config:
     SECRET_KEY = "inf2006"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5ucks@tmdb.cmno6h3huku9.us-east-1.rds.amazonaws.com/task_db" # LOCAL
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pa55w0rdYOU5ucks@tmdb.cmno6h3huku9.us-east-1.rds.amazonaws.com/tmdb" # LOCAL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = "filesystem"  # Store session data in local files
     SESSION_PERMANENT = False  # Session will expire when the browser closes
--- task_db.sql
+++ task_db.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: task_db
+-- Host: 127.0.0.1    Database: tmdb
 -- ------------------------------------------------------
 -- Server version	8.0.41
 
