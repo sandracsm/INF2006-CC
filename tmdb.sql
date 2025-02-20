534fe372191ffa26f39fb0f728256303ca14d87b
itsalys <69067945+itsalys@users.noreply.github.com>
2025-02-21 04:17:57 +0800

    new db schema

--- tmdb.sql
+++ tmdb.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `tmdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tmdb`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: tmdb
 -- ------------------------------------------------------
--- Server version	8.0.41
+-- Server version	8.0.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +16,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admins` (
+  `AdminID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  PRIMARY KEY (`AdminID`),
+  UNIQUE KEY `Email` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admins`
+--
+
+LOCK TABLES `admins` WRITE;
+/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
+INSERT INTO `admins` VALUES (1,'admin','admin@google.com','$2b$12$ul5MH1OBkIJSAFUKUseiLeslDF7LDBmjmOj53IlewLtBfPIkImghi');
+/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `projects`
@@ -28,10 +57,19 @@ CREATE TABLE `projects` (
   `ProjectMembers` varchar(255) DEFAULT NULL,
   `ProjectName` varchar(255) NOT NULL,
   PRIMARY KEY (`ProjectID`),
-  KEY `ProjectOwner` (`ProjectOwner`),
-  CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`ProjectOwner`) REFERENCES `users` (`UserID`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `ProjectOwner` (`ProjectOwner`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (4,2,'2','Ivy\'s Project'),(5,4,'1,2,3,4','Sandra\'s Project'),(6,2,'1,2,3','sandra project 2');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `tasks`
@@ -52,11 +90,19 @@ CREATE TABLE `tasks` (
   `Status` enum('Incomplete','In Progress','Completed','Overdue') DEFAULT 'Incomplete',
   PRIMARY KEY (`TaskID`),
   KEY `ProjectID` (`ProjectID`),
-  KEY `TaskOwner` (`TaskOwner`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`ProjectID`) REFERENCES `projects` (`ProjectID`) ON DELETE CASCADE,
-  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`TaskOwner`) REFERENCES `users` (`UserID`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `TaskOwner` (`TaskOwner`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (7,6,2,'2','sandra project 2 task for ivy','sandra project 2 task for ivy','2025-02-27 23:10:00','Medium','Incomplete'),(9,6,2,'2','fffff','fff','2025-02-28 15:37:00','Low','Incomplete');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -70,10 +116,29 @@ CREATE TABLE `users` (
   `Name` varchar(100) NOT NULL,
   `Email` varchar(100) NOT NULL,
   `Password` varchar(255) NOT NULL,
+  `Disabled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `Email` (`Email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'sandra','sandra@gmail.com','$2b$12$i2ZOsfoHqJ2XWO3RhoAmue4GnAaB8l9Ii9Tmc/xbLF0Qhm4eTYK.i',1),(2,'ivy','ivy@gmail.com','$2b$12$I9kZEL6358KPxqbdJ6tc8OeKceEYXCX7/e1MhGQGykVPrHe3kOoW6',0),(3,'gavin','gavin@gmail.com','$2b$12$41LkaEikDRUAyWY4JODDkuSjTHVdFSeMjOxmVFdHqBGGElDpUvX1S',1),(4,'mandy','mandy@gmail.com','$2b$12$sm/7lDgiL0bv0dlnItLy6OOmI9U8zO/XIUojubx05u4WkRgUo1CkO',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'tmdb'
+--
+
+--
+-- Dumping routines for database 'tmdb'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -84,4 +149,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-02-16 14:25:46
+-- Dump completed on 2025-02-21  2:17:24
